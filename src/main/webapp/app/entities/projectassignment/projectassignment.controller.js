f6b7d93d7ffb86981bdd81a7b7bd42fc7bccb623
Alexander Suchan <e1028089@student.tuwien.ac.at>
2016-12-03 10:14:59 +0100

    Projectassignement: first idea of loading nested entities (show the assigned project id and name)

--- src/main/webapp/app/entities/projectassignment/projectassignment.controller.js
+++ src/main/webapp/app/entities/projectassignment/projectassignment.controller.js
@@ -5,12 +5,13 @@
         .module('seseTranslatorApp')
         .controller('ProjectassignmentController', ProjectassignmentController);
 
-    ProjectassignmentController.$inject = ['$scope', '$state', 'Projectassignment'];
+    ProjectassignmentController.$inject = ['$scope', '$state', 'Projectassignment', 'Project'];
 
-    function ProjectassignmentController ($scope, $state, Projectassignment) {
+    function ProjectassignmentController ($scope, $state, Projectassignment, Project) {
         var vm = this;
 
         vm.projectassignments = [];
+        vm.projects = [];
 
         loadAll();
 
@@ -18,6 +19,21 @@
             Projectassignment.query(function(result) {
                 vm.projectassignments = result;
             });
+
+            Project.query(function(result) {
+                vm.projects = result;
+            });
+        }
+
+        $scope.getAssignedProjectProjectID = getAssignedProjectProjectID;
+
+        function getAssignedProjectProjectID(id) {
+            $scope.assignedProject = null;
+            vm.projects.forEach(function (entry) {
+                if(entry.id == id){
+                    $scope.assignedProject = entry;
+                }
+            })
         }
     }
 })();
