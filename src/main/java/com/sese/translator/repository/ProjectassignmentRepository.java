88dcb892463492e76dffc7620fbcef29ed07e469
Anil Rasouli <e0827794@student.tuwien.ac.at>
2016-12-03 18:30:44 +0100

    show one role of an user in project-details

--- src/main/java/com/sese/translator/repository/ProjectassignmentRepository.java
+++ src/main/java/com/sese/translator/repository/ProjectassignmentRepository.java
@@ -1,16 +1,17 @@
 package com.sese.translator.repository;
 
+import com.sese.translator.domain.Project;
 import com.sese.translator.domain.Projectassignment;
-
-import org.springframework.data.jpa.repository.*;
-
+import com.sese.translator.domain.User;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Spring Data JPA repository for the Projectassignment entity.
  */
 @SuppressWarnings("unused")
-public interface ProjectassignmentRepository extends JpaRepository<Projectassignment,Long> {
+public interface ProjectassignmentRepository extends JpaRepository<Projectassignment, Long> {
 
     @Query("select projectassignment from Projectassignment projectassignment where projectassignment.assignedUser.login = ?#{principal.username}")
     List<Projectassignment> findByAssignedUserIsCurrentUser();
@@ -18,4 +19,7 @@ public interface ProjectassignmentRepository extends JpaRepository<Projectassign
     @Query("SELECT pa FROM Projectassignment pa WHERE pa.assignedProject.owner.login = ?#{principal.username}")
     List<Projectassignment> findByAssignedProjectBelongToCurrentUser();
 
+    List<Projectassignment> findByAssignedProject(Project assignedProject);
+
+    List<Projectassignment> findByAssignedUser(User user);
 }
--- src/main/java/com/sese/translator/web/rest/ProjectResource.java
+++ src/main/java/com/sese/translator/web/rest/ProjectResource.java
@@ -1,21 +1,36 @@
 package com.sese.translator.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
+import com.sese.translator.domain.Projectassignment;
+import com.sese.translator.domain.User;
+import com.sese.translator.domain.enumeration.Projectrole;
+import com.sese.translator.repository.ProjectassignmentRepository;
 import com.sese.translator.service.ProjectService;
+import com.sese.translator.service.UserService;
 import com.sese.translator.service.dto.ProjectDTO;
+import com.sese.translator.service.mapper.ProjectMapper;
+import com.sese.translator.service.mapper.ProjectassignmentMapper;
 import com.sese.translator.web.rest.util.HeaderUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import javax.inject.Inject;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.inject.Inject;
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST controller for managing Project.
@@ -29,6 +44,18 @@ public class ProjectResource {
     @Inject
     private ProjectService projectService;
 
+    @Inject
+    private ProjectMapper projectMapper;
+
+    @Inject
+    private UserService userService;
+    @Inject
+    private ProjectassignmentRepository projectassignmentRepository;
+
+    @Inject
+    private ProjectassignmentMapper projectassignmentMapper;
+
+
     /**
      * POST  /projects : Create a new project.
      *
@@ -41,23 +68,24 @@ public class ProjectResource {
     public ResponseEntity<ProjectDTO> createProject(@Valid @RequestBody ProjectDTO projectDTO) throws URISyntaxException {
         log.debug("REST request to save Project : {}", projectDTO);
         if (projectDTO.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("project", "idexists", "A new project cannot already have an ID")).body(null);
-        }else if(projectDTO.getOwnerId() != null){
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("project", "ownerexists", "A new project cannot already have an Owner")).body(null);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("project", "idexists", "A new project cannot already have an ID")).body(
+                null);
+        } else if (projectDTO.getOwnerId() != null) {
+            return ResponseEntity.badRequest().headers(
+                HeaderUtil.createFailureAlert("project", "ownerexists", "A new project cannot already have an Owner")).body(null);
         }
         ProjectDTO result = projectService.save(projectDTO);
         return ResponseEntity.created(new URI("/api/projects/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert("project", result.getId().toString()))
-            .body(result);
+                             .headers(HeaderUtil.createEntityCreationAlert("project", result.getId().toString()))
+                             .body(result);
     }
 
     /**
      * PUT  /projects : Updates an existing project.
      *
      * @param projectDTO the projectDTO to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated projectDTO,
-     * or with status 400 (Bad Request) if the projectDTO is not valid,
-     * or with status 500 (Internal Server Error) if the projectDTO couldnt be updated
+     * @return the ResponseEntity with status 200 (OK) and with body the updated projectDTO, or with status 400 (Bad Request) if the projectDTO is not valid, or
+     * with status 500 (Internal Server Error) if the projectDTO couldnt be updated
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PutMapping("/projects")
@@ -69,8 +97,8 @@ public class ProjectResource {
         }
         ProjectDTO result = projectService.save(projectDTO);
         return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert("project", projectDTO.getId().toString()))
-            .body(result);
+                             .headers(HeaderUtil.createEntityUpdateAlert("project", projectDTO.getId().toString()))
+                             .body(result);
     }
 
     /**
@@ -98,10 +126,56 @@ public class ProjectResource {
         log.debug("REST request to get Project : {}", id);
         ProjectDTO projectDTO = projectService.findOne(id);
         return Optional.ofNullable(projectDTO)
-            .map(result -> new ResponseEntity<>(
-                result,
-                HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                       .map(result -> new ResponseEntity<>(
+                           result,
+                           HttpStatus.OK))
+                       .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    /**
+     * Get all Users of an Project.
+     *
+     * @param id of the project.
+     * @return a List of Users.
+     */
+    @GetMapping("/projectusers/{id}")
+    @Timed
+    public List<User> getProjectUsers(@PathVariable Long id) {
+
+        List<Projectassignment> projectassignments = projectassignmentRepository.findByAssignedProject(
+            projectMapper.projectDTOToProject(projectService.findOne(id)));
+        // not work with userMapper.userToUserDTO
+        List<User> result = new ArrayList<>();
+
+        for (Projectassignment projectassignment : projectassignments) {
+            result.add(projectassignment.getAssignedUser());
+        }
+
+        return result;
+    }
+
+    /**
+     * Get all Roles of the authentified user for a project.
+     *
+     * @param id of the project.
+     * @return a List of Roles.
+     */
+    @GetMapping("projects/userRole/{id}")
+    @Timed
+    public List<String> getUserRoleForProject(@PathVariable Long id) {
+
+        List<Projectassignment> projectassignments = projectassignmentRepository.findByAssignedUser(userService.getUserWithAuthorities());
+
+        List<String> result = new ArrayList<>();
+
+        for (Projectassignment projectassignment : projectassignments) {
+            if (Objects.equals(projectassignment.getAssignedProject().getId(), id)) {
+                result.add(projectassignment.getRole().name());
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -117,5 +191,4 @@ public class ProjectResource {
         projectService.delete(id);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("project", id.toString())).build();
     }
-
 }
